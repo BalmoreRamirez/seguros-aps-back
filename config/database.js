a39ec1873549febb3878cefbbf00a168ee5c0534
Balmore Ramirez <jbramirez@innovacion.gob.sv>
2025-02-26 08:45:15 -0600

    problema de vercel en vercel.json

--- config/database.js
+++ config/database.js
@@ -6,11 +6,14 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
-  ssl: true,
   port: process.env.DB_PORT,
-  query:{
-    supa:"base-pooler.x"
-  }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // This will ignore self-signed certificates
+    },
+  },
+  logging: false,
 });
 
 export default sequelize;
